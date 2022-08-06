3a0d15d26df0ddad083d2095a6b9968d65269f5a
MGaneshan05 <92172092+MGaneshan05@users.noreply.github.com>
2022-08-05 21:08:35 -0400

    Update Import.py

--- Import.py
+++ Import.py
@@ -5,7 +5,7 @@ url = "https://api-football-v1.p.rapidapi.com/v3/players"
 querystring = {"id":"276","season":"2020"}
 
 headers = {
-	"X-RapidAPI-Key": "bf52d49e93msh937c0f1742ea513p1b23c2jsnfcd110a9c315",
+	"X-RapidAPI-Key": 
 	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
 }
 
