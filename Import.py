6d31e0934e39019f218ec21da6ab07c3143f42ae
MGaneshan05 <92172092+MGaneshan05@users.noreply.github.com>
2022-08-06 11:31:16 -0400

    Update Import.py

--- Import.py
+++ Import.py
@@ -6,7 +6,7 @@ url = "https://api-football-v1.p.rapidapi.com/v3/players"
 querystring = {"league": "61", "season": "2021", "search": "neymar"}
 
 headers = {
-	"X-RapidAPI-Key": "bf52d49e93msh937c0f1742ea513p1b23c2jsnfcd110a9c315",
+	"X-RapidAPI-Key": 
 	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
 }
 
