bcf889f4149b664d950ddfa450f0b81ab2e022a8
MGaneshan05 <milan.ganeshan@gmail.com>
2022-08-05 21:02:44 -0400

    ver 1.0

--- Import.py
+++ Import.py
@@ -1,1 +1,14 @@
+import requests
 
+url = "https://api-football-v1.p.rapidapi.com/v3/players"
+
+querystring = {"id":"276","season":"2020"}
+
+headers = {
+	"X-RapidAPI-Key": "bf52d49e93msh937c0f1742ea513p1b23c2jsnfcd110a9c315",
+	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
