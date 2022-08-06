2db80600671ac739c7f806174f9affbe0003904a
MGaneshan05 <milan.ganeshan@gmail.com>
2022-08-06 11:12:43 -0400

    ver 2.0

--- Import.py
+++ Import.py
@@ -1,11 +1,12 @@
 import requests
+import USERINPUT
 
 url = "https://api-football-v1.p.rapidapi.com/v3/players"
 
-querystring = {"id":"276","season":"2020"}
+querystring = {"league": "61", "season": "2021", "search": "neymar"}
 
 headers = {
-	"X-RapidAPI-Key": 
+	"X-RapidAPI-Key": "bf52d49e93msh937c0f1742ea513p1b23c2jsnfcd110a9c315",
 	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
 }
 
---
+++ USERINPUT.py
@@ -0,0 +1,9 @@
+def user_input():
+    print("Please type the desired league:")
+    lg = input()
+    print("Please type the desired season")
+    szn = input()
+    print("Please type the desired player")
+    plyr = input()
+
+
